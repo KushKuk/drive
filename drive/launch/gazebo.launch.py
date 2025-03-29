8ebaa3df6a7a1a30b83f259ef1f1140cbcf7001e
KushKuk <kukrejakush@gmail.com>
2025-03-29 17:55:22 +0530

    base_link

--- drive/launch/gazebo.launch.py
+++ drive/launch/gazebo.launch.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import os
 
 from ament_index_python.packages import get_package_share_directory
@@ -23,7 +22,7 @@ def generate_launch_description():
             package='tf2_ros',
             executable='static_transform_publisher',
             name='tf_footprint_base',
-            arguments=['0', '0', '0', '0', '0', '0', 'base_link', 'base_footprint']
+            arguments=['0', '0', '0', '0', '0', '0', 'base_footprint', 'base_link']
         ),
         Node(
             package='gazebo_ros',
