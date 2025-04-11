e8335e70785d4ba72ae0fd0765c723ea27ed8623
KushKuk <kukrejakush@gmail.com>
2025-04-11 13:34:52 +0530

    world

--- drive/launch/gazebo.launch.py
+++ drive/launch/gazebo.launch.py
@@ -7,18 +7,22 @@ from launch.actions import ExecuteProcess
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    # Get the absolute path to the drive package
     drive_share_dir = get_package_share_directory('drive')
+
+    # World path (make sure filename matches exactly)
+    world_path = os.path.join(drive_share_dir, 'worlds', 'world.sdf')
+
+    # URDF path
     urdf_path = os.path.join(drive_share_dir, 'models', 'drive', 'urdf', 'drive.urdf')
 
     return LaunchDescription([
-        # Launch Ignition Gazebo with empty world
+        # Launch Ignition with your custom world
         ExecuteProcess(
-            cmd=['ign', 'gazebo', '-r', 'empty.sdf'],
+            cmd=['ign', 'gazebo', '-r', world_path],
             output='screen'
         ),
 
-        # Start robot_state_publisher with URDF
+        # robot_state_publisher
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
@@ -29,7 +33,7 @@ def generate_launch_description():
             output='screen'
         ),
 
-        # Spawn robot in Ignition using ros_ign_gazebo
+        # Spawn robot using ros_ign_gazebo
         Node(
             package='ros_ign_gazebo',
             executable='create',
